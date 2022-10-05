ec61680fe499d8618825cfe3e6636e5cfbc01223
LukasMansoreitch <lukasmansoreitch@hotmail.com>
2022-10-05 20:19:09 -0300

    aaa

--- backend/banco.js
+++ backend/banco.js
@@ -1,17 +1,21 @@
 
  module.exports = class voos{
 
-     async buscar() {
+        buscar() {
         const {MongoClient} = require('mongodb');
         const url = "mongodb://localhost:27017";
         const client = new MongoClient(url);
-        await client.connect();
+        client.connect().then(() => {
+            console.log("conectado ao banco")
+        }).catch((err) => {
+            console.log("aconteceu um erro")
+        });
         const db = client.db("Voos");
         const usuarios = db.collection("Viagens");
 
-        const result = usuarios.find({}).toArray();
-        return result;  
-        }  
+        const result = usuarios.find({}).toArray(function(result, err) {
+            if (err) throw (result)
+            console.log(result)
+        })
     }
-
-
+}
--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const {MongoClient} = require('mongodb');
+const {MongoClient} = require('mongodb')
+const voos = require("./banco.js")
 var cors = require('cors')
 const app = express()
 const port = 3000
 
-const voos = require("./banco.js")
+const banco = new voos();
 app.use(cors())
 
-app.get("/buscar")
- const result = voos.buscar()
-console.log(result)
+app.get("/buscar", () => {
+  const result = banco.buscar()
+  console.log(result)
+});
+
 
 app.listen(port, () => {
     console.log(`servidor aberto na porta : ${port}`)
